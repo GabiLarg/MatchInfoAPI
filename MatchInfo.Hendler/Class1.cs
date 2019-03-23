737867521b854f396d096867ba7be1322ec4696d
Gabriela Assis <gabygufs@gmail.com>
2019-03-23 14:46:26 -0300

    Initial commit - Project Structure

---
+++ MatchInfo.Hendler/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MatchInfo.Hendler
+{
+    public class Class1
+    {
+    }
+}
---
+++ MatchInfo.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MatchInfo.Repository
+{
+    public class Class1
+    {
+    }
+}
